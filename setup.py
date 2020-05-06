8b26a7c0d367c831659d3257dd8110eccec97a3a
Reid 'arrdem' McKenzie <me@arrdem.com>
2020-05-05 22:23:40 -0700

    Shuffle to allow multi-src

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ from setuptools import setup, find_packages
 setup(
     name='calf',
     version='0.0.0',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    install_requires=[
-        'six>=1.10.0'
-    ]
+    package_dir={'': 'src/python'},
+    packages=find_packages(where='src/python'),
 )
