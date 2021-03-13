7ea80079d417d7d7ea028857f876da77a2200514
Reid 'arrdem' McKenzie <me@arrdem.com>
2021-03-13 07:52:07 -0700

    Create a bunch of entrypoints

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 
+from os import path
+
 from setuptools import setup, find_packages
 
+
+# Fetch the README contents
+rootdir = path.abspath(path.dirname(__file__))
+with open(path.join(rootdir, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
 setup(
-    name='calf',
-    version='0.0.0',
-    package_dir={'': 'src/python'},
-    packages=find_packages(where='src/python'),
+    name="calf",
+    version="0.0.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src/python"},
+    packages=find_packages(where="src/python"),
+    entry_points={
+        "console_scripts": [
+            "calflex = calf.lexer:main",
+            "calfp = calf.parser:main",
+            "calfr = calf.reader:main",
+            "calffmt = calf.fmt:main",
+            "calf = calf.server:main",
+        ]
+    },
 )
